b2875b68e9e4fbdec6c2a9d24748317f2719bfb1
duggerd <duggerd@gmail.com>
2012-05-25 22:18:03 -0300

    Formatted JS Code

--- hidediv.js
+++ hidediv.js
@@ -1,10 +1,10 @@
 var lastDiv = "";
 function showDiv(divName) {
-	// hide last div
+
 	if (lastDiv) {
 		document.getElementById(lastDiv).className = "hiddenDiv";
 	}
-	//if value of the box is not nothing and an object with that name exists, then change the class
+
 	if (divName && document.getElementById(divName)) {
 		document.getElementById(divName).className = "visibleDiv";
 		lastDiv = divName;
