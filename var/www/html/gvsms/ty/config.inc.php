13d62b28ebd0ad015cfb7c24de8fb1d35058950f
RossIV <rosslindsayiv@gmail.com>
2012-09-07 22:40:58 -0400

    Add config.inc.php for TYSMS

---
+++ var/www/html/gvsms/ty/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+$username = 'root';
+$password = 'passw0rd';
+$tydatabase = 'teleyapper';
+$server = 'localhost';
+$gvoiceuser = 'youremail@gmail.com';
+$gvoicepass = 'yourpassword';
+$company = 'Company'
+?>
